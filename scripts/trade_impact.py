fa227187a021f9f683fca62580c80cc96a080e6c
Jorge Rivera <jorge.rivera@one.org>
2022-04-22 16:27:28 +0200

    new crude chart

--- scripts/trade_impact.py
+++ scripts/trade_impact.py
@@ -9,6 +9,7 @@ import country_converter as coco
 from scripts.commodities_analysis import get_commodity_prices
 
 TO_BARRELS: float = 0.1364
+CRUDE_COUNTRIES = ["NGA", "AGO", "LBY", "DZA", "COG", "EGY"]
 
 
 def _read_raw_data():
@@ -79,9 +80,23 @@ def _yearly_average(df, exclude: list = None):
 
     grouper = [x for x in df.columns if x not in exclude]
 
-    years = f"{df.year.min()}-{df.year.max()}"
+    years = f"{df.year.min()}-{df.year.max()} (mean)"
 
     return df.groupby(grouper).mean().reset_index().assign(year=years)
+
+
+def _calc_mean_prices(
+    yearly_prices: pd.DataFrame, start_year: int, end_year: int
+) -> dict:
+    """Calculate the mean price for each commodity"""
+    return (
+        yearly_prices.loc[
+            (yearly_prices.year >= start_year) & (yearly_prices.year <= end_year)
+        ]
+        .groupby(["commodity"])
+        .mean()["value"]
+        .to_dict()
+    )
 
 
 def read_filtered_grouped_data() -> pd.DataFrame:
@@ -116,50 +131,40 @@ def read_filtered_grouped_data() -> pd.DataFrame:
     return df
 
 
-def get_african_imports(df: pd.DataFrame) -> pd.DataFrame:
+def get_african_imports(df: pd.DataFrame, yearly: bool = False) -> pd.DataFrame:
     """STEP 2: Filter for African imports"""
-    return (
+    df = (
         df.pipe(_only_african_imports)
         .groupby(
-            [
-                "year",
-                "importer",
-                "category",
-                "pink_sheet_commodity",
-            ],
-            as_index=False,
+            ["year", "importer", "category", "pink_sheet_commodity"], as_index=False,
         )
         .sum()
-        .pipe(_yearly_average, exclude=["year", "quantity"])
-        .rename(
-            columns={
-                "quantity": "imports_quantity",
-                "importer": "iso_code",
-            }
-        )
     )
 
+    if not yearly:
+        df = df.pipe(_yearly_average, exclude=["year", "quantity"])
 
-def get_african_exports(df: pd.DataFrame) -> pd.DataFrame:
+    return df.rename(columns={"quantity": "imports_quantity", "importer": "iso_code"})
+
+
+def get_african_exports(df: pd.DataFrame, yearly: bool = False) -> pd.DataFrame:
     """STEP 3: Filter for African exports"""
-    return (
+    df = (
         df.pipe(_only_african_exports)
         .groupby(
             ["year", "exporter", "category", "pink_sheet_commodity"], as_index=False
         )
         .sum()
-        .pipe(_yearly_average, exclude=["year", "quantity"])
-        .assign(quantity=lambda d: -1 * d.quantity)
-        .rename(
-            columns={
-                "quantity": "exports_quantity",
-                "exporter": "iso_code",
-            }
-        )
+    )
+    if not yearly:
+        df = df.pipe(_yearly_average, exclude=["year", "quantity"])
+
+    return df.assign(quantity=lambda d: -1 * d.quantity).rename(
+        columns={"quantity": "exports_quantity", "exporter": "iso_code"}
     )
 
 
-def get_net_imports(
+def calc_net_imports(
     imports_df: pd.DataFrame, exports_df: pd.DataFrame, column_to_net: str = "quantity"
 ) -> pd.DataFrame:
     """STEP 4: Calculate net imports"""
@@ -233,21 +238,98 @@ def get_spending(
     )
 
 
-def pipeline():
+def get_net_imports_africa(yearly: bool = False) -> pd.DataFrame:
+    """A clean version of the dataset,filtered for Africa"""
 
     # Read a clean version of the full dataset (quantities)
     df = read_filtered_grouped_data()
 
     # African imports
-    afr_imp = get_african_imports(df)
+    afr_imp = get_african_imports(df, yearly=yearly)
 
     # African exports
-    afr_exp = get_african_exports(df)
+    afr_exp = get_african_exports(df, yearly=yearly)
 
-    # STEP 4: Combine imports and exports data and calculate net imports quantity
-    data = get_net_imports(
+    # Combine imports and exports data and calculate net imports quantity
+    return calc_net_imports(
         imports_df=afr_imp, exports_df=afr_exp, column_to_net="quantity"
     )
+
+
+def crude_evolution_chart() -> None:
+    """A Flourish chart to visualise additional revenue from net crude exports, for
+    selected countries"""
+
+    df = get_net_imports_africa()
+
+    # filter for crude
+    df = (
+        df.loc[lambda d: (d.category == "crude oil") & (d.net_imports_quantity < 0)]
+        .loc[lambda d: d.iso_code.isin(CRUDE_COUNTRIES)]
+        .assign(net_exports_quantity=lambda d: -d.net_imports_quantity)
+        .reset_index(drop=True)
+    )
+    # Get yearly prices
+    yearly_prices = get_yearly_prices_data(["Crude oil, average"])
+
+    # Convert to mean prices dict
+    mean_18_20_prices = yearly_prices.pipe(_calc_mean_prices, 2018, 2020)
+
+    # Get latest prices
+    latest_prices = get_latest_prices_data(["Crude oil, average"])
+
+    # Calculate spending:
+    df = df.pipe(
+        get_spending,
+        units_columns=["net_exports_quantity"],
+        prices=[("pre_crisis", mean_18_20_prices), ("latest", latest_prices)],
+    ).assign(
+        value_net_exports_impact=lambda d: d.value_net_exports_latest
+        - d.value_net_exports_pre_crisis
+    )
+
+    # Calculate spending by population
+    df = df.pipe(utils.add_population).assign(
+        value_exp_pre_pp=lambda d: d.value_net_exports_pre_crisis / d.population,
+        value_exp_latest_pp=lambda d: d.value_net_exports_latest / d.population,
+    )
+
+    # Clean for export
+    df = (
+        df.assign(country=lambda d: coco.convert(d.iso_code, to="short_name"))
+        .filter(
+            [
+                "country",
+                "net_exports_quantity",
+                "value_net_exports_pre_crisis",
+                "value_net_exports_latest",
+                "value_net_exports_impact",
+                "value_exp_latest_pp",
+            ],
+            axis=1,
+        )
+        .assign(
+            net_exports_quantity=lambda d: d.net_exports_quantity / 1e6,
+            value_net_exports_pre_crisis=lambda d: d.value_net_exports_pre_crisis / 1e9,
+            value_net_exports_latest=lambda d: d.value_net_exports_latest / 1e9,
+            value_net_exports_impact=lambda d: d.value_net_exports_impact / 1e9,
+        )
+        .rename(
+            columns={
+                "net_exports_quantity": "Net exports (million barrels)",
+                "value_net_exports_pre_crisis": "Average revenue (pre-war)",
+                "value_net_exports_latest": "Potential revenue (current prices)",
+                "value_net_exports_impact": "Potential additional revenue",
+                "value_exp_latest_pp": "Potential revenue per capita (current prices)",
+            }
+        )
+    )
+
+
+def pipeline():
+
+    # Combine imports and exports data and calculate net imports quantity
+    data = get_net_imports_africa()
 
     # Add total trade
     data = data.pipe(add_total_trade)
@@ -257,12 +339,7 @@ def pipeline():
 
     # Get yearly average prices
     yearly_prices = get_yearly_prices_data(commodities)
-    mean_18_20_prices = (
-        yearly_prices.loc[(yearly_prices.year >= 2018) & (yearly_prices.year <= 2020)]
-        .groupby(["commodity"])
-        .mean()["value"]
-        .to_dict()
-    )
+    mean_18_20_prices = yearly_prices.pipe(_calc_mean_prices, 2018, 2020)
 
     # Get the latest (monthly) prices
     latest_prices = get_latest_prices_data(commodities)
@@ -318,5 +395,6 @@ def pipeline():
 
 
 if __name__ == "__main__":
-    analysis = pipeline()
-    analysis.to_csv(paths.output + r"/data_for_analysis.csv", index=False)
+    pass
+    # analysis = pipeline()
+    # analysis.to_csv(paths.output + r"/data_for_analysis.csv", index=False)
