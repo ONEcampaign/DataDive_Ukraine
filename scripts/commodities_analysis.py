30021a697aac2bd1bc621a7d90370678df266c68
lpicci96 <lpicci96@gmail.com>
2022-05-11 12:26:06 -0600

    add index chart

--- scripts/commodities_analysis.py
+++ scripts/commodities_analysis.py
@@ -9,6 +9,7 @@ Values are in nominal USD
 import pandas as pd
 from scripts import config
 from numpy import nan
+from typing import Optional
 
 # ==================  PARAMETERS ======================
 
@@ -20,6 +21,7 @@ COMMODITY_URL = (
 )
 
 COMMODITY_DATA = pd.read_excel(COMMODITY_URL, sheet_name="Monthly Prices")
+INDEX_DATA = pd.read_excel(COMMODITY_URL, sheet_name="Monthly Indices")
 # =======================================================
 
 
@@ -48,6 +50,30 @@ def get_commodity_prices(commodities: list) -> pd.DataFrame:
     return df
 
 
+def get_indices(indices: Optional[list] =None) -> pd.DataFrame:
+    """gets index data from World Bank and returns a clean dataframe"""
+
+    df = INDEX_DATA.copy()
+
+    df = (df.iloc[9:]
+          .reset_index(drop=True)
+          .replace("..", nan))
+    df.columns = ['period','Energy', 'Non-energy', 'Agriculture', 'Beverages', 'Food',
+                  'Oils & Meals', 'Grains', 'Other Food', 'Raw Materials','Timber',
+                  'Other Raw Mat.', 'Fertilizers', 'Metals & Minerals',
+                  'Base Metals (ex. iron ore)', 'Precious Metals']
+
+    #filter indices
+    if indices is not None:
+        indices.insert(0, 'period') # add column name for period
+        df = df.loc[:, indices]
+
+    #change date format
+    df['period'] = pd.to_datetime(df.period, format="%YM%m")
+
+    return df
+
+
 def create_commodity_chart_data(df: pd.DataFrame, start_date: str) -> None:
     """Filters the data for selected start date.
     Creates a CSV formatted for Flourish."""
@@ -59,13 +85,26 @@ def create_commodity_chart_data(df: pd.DataFrame, start_date: str) -> None:
     print("Successfully created commodity chart")
 
 
+def create_index_chart(df: pd.DataFrame, start_date = str) -> None:
+    """Filters the data for selected dated and creates a csv formatted for flourish"""
+
+    (df.loc[df.period >= start_date]
+     .reset_index(drop=True)
+     .to_csv(f'{config.paths.output}/indices.csv', index=False))
+
+    print('Successfully created index chart')
+
+
 def update_data() -> None:
     """Pipeline to update the data"""
-    # Get the commodity data
-    data = get_commodity_prices(commodities=COMMODITY_LIST)
+    # Get commodity and index data
+    commodity_data = get_commodity_prices(commodities=COMMODITY_LIST)
+    index_data = get_indices()
+
 
     # create chart csv
-    create_commodity_chart_data(df=data, start_date="2018-01-01")
+    create_commodity_chart_data(df=commodity_data, start_date="2018-01-01")
+    create_index_chart(df = index_data, start_date="2000-01-01")
 
 
 if __name__ == "__main__":
